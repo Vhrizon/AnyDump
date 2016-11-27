7d15a5ae010175f7cc119fa83fb61c3b7b7b8a40
Donutttt <orr2@live.com>
2016-11-27 23:25:41 +0000

    updated a few things and added comments, should now work

--- ACTUAl.py
+++ ACTUAl.py
@@ -1,39 +1,47 @@
-#test
-
-
-
-
 #--------------------------------------------------------------
 
-def Menu():
+# menu needed to have file contents passed into it
+# otherwise it wouldn't know about it, variables
+# defined in python functions are scoped to that function
+def menu(file_contents):
 
     print("Now we have your text file, what would you like do do?\n1.Convert to ascii\n2.Close")
-    if input() == "1":
-        Ascii()
-    elif input() == "2":
+
+    # best to make sure we get a string, so we know what we're working with
+    input_value = str(raw_input())
+
+    if input_value == "1":
+        ascii(file_contents)
+    elif input_value == "2":
         f.close()
     else:
         print("ok")
         
 #--------------------------------------------------------------
 
-def Start():
+def start():
 
     print("please give the directory and name of text file exactly")
 
-    x = input()                                     
-                                                
+    # this probably depends on your version of python,
+    # input() is python 3 only I think
+    # raw_input achieves the same more or less in v2.x
+    x = raw_input()                                     
+
+    # this way of using %s to add variables into strings can be helpful
+    print('value of x: %s' % (x))
+
     f = open(x, 'r')                                
                                                 
     file_contents = f.read()                        
                                                 
-    print(file_contents)                            
+    print('file contents %s' % (file_contents))                            
 
-    Menu()
+    menu(file_contents)
 
 #--------------------------------------------------------------
 
-def Ascii(file_contents):
+def ascii(file_contents):
     
     asciitime = [ord(c) for c in file_contents]
     str1 = ''.join(str(e) for e in file_contents)
@@ -42,6 +50,6 @@ def Ascii(file_contents):
     print(len(asciitime))
     print(str1)
     
-Start()
+start()
 
 
