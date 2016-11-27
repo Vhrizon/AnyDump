72d69880e9ea5fe06cc07adb50bcbce2101915e1
Vhrizon <radiniboy@live.co.uk>
2016-11-27 23:14:18 +0000

    Update ACTUAl.py

--- ACTUAl.py
+++ ACTUAl.py
@@ -1,3 +1,47 @@
 #test
 
-print(chr(97))
+
+
+
+#--------------------------------------------------------------
+
+def Menu():
+
+    print("Now we have your text file, what would you like do do?\n1.Convert to ascii\n2.Close")
+    if input() == "1":
+        Ascii()
+    elif input() == "2":
+        f.close()
+    else:
+        print("ok")
+        
+#--------------------------------------------------------------
+
+def Start():
+
+    print("please give the directory and name of text file exactly")
+
+    x = input()                                     
+                                                
+    f = open(x, 'r')                                
+                                                
+    file_contents = f.read()                        
+                                                
+    print(file_contents)                            
+
+    Menu()
+
+#--------------------------------------------------------------
+
+def Ascii(file_contents):
+    
+    asciitime = [ord(c) for c in file_contents]
+    str1 = ''.join(str(e) for e in file_contents)
+    
+    print(asciitime)
+    print(len(asciitime))
+    print(str1)
+    
+Start()
+
+
